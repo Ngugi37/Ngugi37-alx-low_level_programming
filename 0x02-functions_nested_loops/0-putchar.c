c2d3975ca61fea319fc3ae6a1373b0628bf1ab55
Ngugi37 <ngugimaurice17@gmail.com>
2023-06-20 16:25:02 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,22 +1,20 @@
 #include "main.h"
-	/**
-	 * main - print _putchar
-	 * is written by mbah
-	 * Return: 0
-	 */
+/**
+ * main - Print "main" followed by a new line.
+ * Description: You are not allowed to include standard libraries.
+ * Return: 0
+ */
+int main(void)
+{
+	char main[] = "main";
+	int i = 0;
 
-
-	int main(void)
+	while (main[i] != '\0')
 	{
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
-	return (0);
+		_putchar(main[i]);
+		i++;
 	}
+	_putchar('\n');
 
+	return (0);
+}
